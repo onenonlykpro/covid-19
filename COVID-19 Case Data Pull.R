697a4d745e4e363fc4b339e170b2ed4f660a50f2
Kyle Protho <Onenonlykpro@gmail.com>
2020-03-21 09:05:18 -0400

    Main script

---
+++ COVID-19 Case Data Pull.R
@@ -0,0 +1,8 @@
+
+# Get global case data -------------------------------------------------
+source("C:/Users/oneno/OneDrive/UPMC/Scripts/COVID-19 Case Data Pull - Global.R")
+
+
+# Create and export dataframe of sources ----------------------------------
+source("C:/Users/oneno/OneDrive/UPMC/Scripts/COVID-19 Sources.R")
+
