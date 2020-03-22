22015640b3548603282e40211fea90965ac9b5d5
Kyle Protho <Onenonlykpro@gmail.com>
2020-03-22 11:23:43 -0400

    West Virginia case extraction

--- COVID-19 Case Data Pull.R
+++ COVID-19 Case Data Pull.R
@@ -6,8 +6,8 @@ source("C:/Users/oneno/OneDrive/UPMC/Scripts/COVID-19 Case Data Pull - Global.R"
 # Get case data by county of states of interest ------------------------------------------------
 source("C:/Users/oneno/OneDrive/UPMC/Scripts/COVID-19 Case Data Pull - Pennsylvania.R")
 source("C:/Users/oneno/OneDrive/UPMC/Scripts/COVID-19 Case Data Pull - Ohio.R")
+source("C:/Users/oneno/OneDrive/UPMC/Scripts/COVID-19 Case Data Pull - West Virginia.R")
 
 
 # Create and export dataframe of sources ----------------------------------
 source("C:/Users/oneno/OneDrive/UPMC/Scripts/COVID-19 Sources.R")
-
